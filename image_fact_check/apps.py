ee4e7f8343e5a34f016e254112b66d798f9d9bbd
Islam Badran <islambadran39@gmail.com>
2025-11-06 05:58:22 +0200

    fix sending

--- image_fact_check/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImageFactCheckConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'image_fact_check'
--- image_fact_check/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ImageFactCheckView
-
-urlpatterns = [
-    path("", ImageFactCheckView.as_view(), name="image-fact-check"),
-]
-
