cce189f45d75dad9ffb4fd55110e4fcbc5059d05
Islam Badran <islambadran39@gmail.com>
2025-12-24 08:31:02 +0200

    update route

--- Config/urls.py
+++ Config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fact_check/', include('fact_check_with_openai.urls')),
+    path('palastine-fact_check/', include('fact_check_with_openai.urls')),
 ]
